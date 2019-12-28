fbe449666c6153e1f1f9f2b6ff1e2038d5ce0d56
Santosh Vaza <vazasantosh@gmail.com>
2019-12-28 08:58:20 +0000

    configure project for leetcode

---
+++ leetcode/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Writeline("Hello World");
+        }
+    }
+}
