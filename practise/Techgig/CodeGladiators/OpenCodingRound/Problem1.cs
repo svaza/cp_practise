a40cc1349dd05c4e6e71ed141c0951dd229ec897
Santosh Vaza <11908578+svaza@users.noreply.github.com>
2020-04-25 19:50:19 +0530

    techgig - solve opencoding round problem

---
+++ practise/Techgig/CodeGladiators/OpenCodingRound/Problem1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Practise.Techgig.CodeGladiators.OpenCodingRound
+{
+    public class Problem1
+    {
+        public static void Main(String[] args)
+        {
+            int numberOfIngredients = int.Parse(Console.ReadLine());
+            long[] quantityReq = Console.ReadLine()
+                .Trim()
+                .Split(' ')
+                .Select(e => long.Parse(e))
+                .ToArray();
+
+            long[] quantityHas = Console.ReadLine()
+                .Trim()
+                .Split(' ')
+                .Select(e => long.Parse(e))
+                .ToArray();
+
+            long prevProducable = quantityHas[0] / quantityReq[0];
+            for (int i = 1; i < numberOfIngredients; i++)
+            {
+                long producable = quantityHas[i] / quantityReq[i];
+                if (prevProducable > producable)
+                {
+                    prevProducable = producable;
+                }
+            }
+
+            Console.WriteLine(prevProducable);
+        }
+    }
+}
---
+++ practise/Techgig/CodeGladiators/OpenCodingRound/Problem2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace Practise.Techgig.CodeGladiators.OpenCodingRound
+{
+    public class Problem2
+    {
+        static void Main(String[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (--t >= 0)
+            {
+                int noOfMembers = int.Parse(Console.ReadLine());
+
+                var grev = Console.ReadLine().Trim()
+                    .Split(' ')
+                    .Select(e => long.Parse(e))
+                    .OrderBy(e => e)
+                    .ToArray();
+
+                var opponent = Console.ReadLine().Trim()
+                    .Split(' ')
+                    .Select(e => long.Parse(e))
+                    .OrderBy(e => e)
+                    .ToArray();
+
+                int counter = 0;
+                int pointer = 0;
+                foreach(var opponentPower in opponent)
+                {
+                    while(pointer < grev.Length)
+                    {
+                        if (grev[pointer] > opponentPower)
+                        {
+                            // Console.WriteLine($"{grev[pointer]} cancelled {opponentPower}");
+                            counter++;
+                            pointer++;
+                            break;
+                        }
+                        pointer++;
+                    }
+
+                    if(pointer >= grev.Length)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
